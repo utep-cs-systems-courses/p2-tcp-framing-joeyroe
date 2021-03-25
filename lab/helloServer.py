51959ef03d512a18da8ea379a3a9b14c7c2373a2
joeyroe <ljroe@miners.utep.edu>
2021-03-25 00:45:50 -0600

    server sends msg to client

---
+++ lab/helloServer.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python3
+
+# Echo server program
+
+import socket, sys, re, os
+sys.path.append("../lib")       # for params
+import params
+from FramedSocket import *
+
+switchesVarDefaults = (
+    (('-l', '--listenPort') ,'listenPort', 50001),
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+
+
+progname = "echoserver"
+paramMap = params.parseParams(switchesVarDefaults)
+
+listenPort = paramMap['listenPort']
+listenAddr = ''       # Symbolic name meaning all available interfaces
+
+if paramMap['usage']:
+    params.usage()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((listenAddr, listenPort))
+s.listen(1)              # allow only one outstanding request
+# s is a factory for connected sockets
+
+
+
+
+
+while True:
+    conn, addr = s.accept() # wait until incoming connection request (and accept it)
+    
+    framedSocket = FramedSocket(conn)
+    
+    if os.fork() == 0:      # child becomes server
+        print('Connected by', addr)
+        fileName = input()
+
+        transferFile = open(fileName, 'rb')
+        fileList = transferFile.readlines()
+
+        conn.send(bytes(str(len(fileList)), 'utf-8')) #send the number of lines in file
+
+        for i in range(len(fileList)):
+            framedSocket.framedSend(fileList[i])
+
+            if(conn.recv(1024).decode() == "No"): #if client doesn't receive a packet
+                frameSocket.framedSend(fileList[i]) #resend it
+
+
